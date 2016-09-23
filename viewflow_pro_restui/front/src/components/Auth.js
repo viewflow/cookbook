6996e72ef8483daef9c13ee1611f89fe5b973e6e
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2016-09-23 11:13:27 +0600

    Improve auth form

--- viewflow_pro_restui/front/src/components/Auth.js
+++ viewflow_pro_restui/front/src/components/Auth.js
@@ -1,10 +1,27 @@
 import React, { Component, PropTypes } from 'react'
 
+import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import {Card, CardActions, CardTitle, CardText} from 'material-ui/Card';
 
 import auth from '../api/auth'
 
+const styles = {
+  overlay: {
+    position: 'fixed',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignContent: 'center',
+    background: '#eee',
+    height: '100%',
+    width:'100%',
+  },
+  formField: {
+    width: '100%',
+  }
+}
 
 class Auth extends Component {
   getChildContext() {
@@ -13,18 +30,25 @@ class Auth extends Component {
 
   renderLoginScreen() {
     return (
-      <div>
+      <div style={styles.overlay}>
         <form>
-          <TextField floatingLabelText="Username" />
-          <TextField floatingLabelText="Password" type="password" />
-          <RaisedButton label="Login" primary={true} />
+          <Card>
+            <CardTitle title="Login" subtitle="Please, input your credentials"/>
+            <CardText>
+              <TextField floatingLabelText="Username" style={styles.formField}/>
+              <TextField floatingLabelText="Password" type="password"  style={styles.formField}/>
+            </CardText>
+            <CardActions>
+              <RaisedButton label="Login" primary={true}  fullWidth={true}/>
+            </CardActions>
+          </Card>
         </form>
       </div>
     )
   }
 
   render() {
-    return auth.loggedIn() ? <div>{this.props.children}</div> : this.renderLoginScreen()
+    return !auth.loggedIn() ? <div>{this.props.children}</div> : this.renderLoginScreen()
   }
 }
 
