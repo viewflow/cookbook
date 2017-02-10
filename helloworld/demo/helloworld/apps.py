a0c2256f9d5c24da2e0902aefb770fb3231108bb
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2017-02-10 12:54:49 +0600

    Fresh hello world sample

---
+++ helloworld/demo/helloworld/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HellowoldConfig(AppConfig):
+    name = 'hellowold'
---
+++ helloworld/demo/helloworld/flows.py
@@ -0,0 +1,45 @@
+from viewflow import flow, frontend
+from viewflow.base import this, Flow
+from viewflow.flow.views import CreateProcessView, UpdateProcessView
+
+from .models import HelloWorldProcess
+
+
+@frontend.register
+class HelloWorldFlow(Flow):
+    process_class = HelloWorldProcess
+
+    start = (
+        flow.Start(
+            CreateProcessView,
+            fields=["text"]
+        ).Permission(
+            auto_create=True
+        ).Next(this.approve)
+    )
+
+    approve = (
+        flow.View(
+            UpdateProcessView,
+            fields=["approved"]
+        ).Permission(
+            auto_create=True
+        ).Next(this.check_approve)
+    )
+
+    check_approve = (
+        flow.If(lambda activation: activation.process.approved)
+        .Then(this.send)
+        .Else(this.end)
+    )
+
+    send = (
+        flow.Handler(
+            this.send_hello_world_request
+        ).Next(this.end)
+    )
+
+    end = flow.End()
+
+    def send_hello_world_request(self, activation):
+        print(activation.process.text)
