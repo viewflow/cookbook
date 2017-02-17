243e173e6982892e2738cbcd2929a9ff604f9527
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2017-02-17 10:13:36 +0600

    Viewflow 0.12.2.1 became compatible with Celery 3.x

--- celery/demo/celery.py
+++ celery/demo/celery.py
@@ -1,17 +1,18 @@
 from __future__ import absolute_import, unicode_literals
 import os
-from celery import Celery
+import celery
+
+from django.conf import settings
+
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo.settings')
 
-app = Celery('demo')
+app = celery.Celery('demo')
 
-# Using a string here means the worker don't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+if celery.VERSION < (4, 0):
+     app.config_from_object('django.conf:settings')
+     app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+else:
+    app.config_from_object('django.conf:settings', namespace='CELERY')
+    app.autodiscover_tasks()
