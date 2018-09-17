2451ecc500c70f14eee765e7efeb2483995954e5
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2018-09-17 12:56:46 +0600

    Starting modern django tutorial

---
+++ _articles/modern_django/basic/polls/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
